f560b94ba05c20c5c408ddea8070947b6cdabddc
bally4h <banfibalazs81@gmail.com>
2023-02-27 20:01:54 +0100

    Rock Paper Scissors 5 times loop at the en with the points and the winner

--- myscript.js
+++ myscript.js
@@ -32,44 +32,46 @@ function playRound(playerSelection, computerSelection) {
       }
     }
 else {
-    return "Not valid! Choose only Rock, Paper or Scissors!";
+    return "Not";
 }
 }
 
-   
-  
+ 
   function game() {
     let pointPlay = 0;
     let pointComp = 0;  
     for (let i = 0; i < 5; i++) {
-      const playerSelection2 = prompt("Please enter your choice: Rock, Paper or Scissors:");
-      console.log(playerSelection2);
+      const playerSelection = prompt("Please enter your choice: Rock, Paper or Scissors:");
       const computerSelection = getComputerChoice();
-      console.log(playRound(playerSelection2, computerSelection));
-      let won = playRound(playerSelection2, computerSelection);
+      let won = playRound(playerSelection, computerSelection);
+      console.log("Player selection: " + playerSelection + " Computer selection: " + computerSelection);
 
       switch (won) {
         case "Tie":
-          console.log("Tie!");
-          break;
+          console.log("Tie! New turn!");
+          i--;
+          break;          
         case "Not":
-          console.log("Not valid! Choose only Rock, Paper or Scissors!");
+          console.log("Not valid! Choose only Rock, Paper or Scissors! New turn!");
+          i--;
           break;
         case "player":
-          console.log("You won! " + playerSelection + " beats " + computerSelection")
+          console.log("You won! " + playerSelection + " beats " + computerSelection);
           pointPlay += 1;
           break;
         case "comp":
-          console.log("You lose! " + computerSelection + " beats " + playerSelection");
+          console.log("You lose! " + computerSelection + " beats " + playerSelection);
           pointComp += 1;
           break;
       }
-      
-          
-
-      console.log(point);
-
+      console.log("Round: " + (i+1) + ". Player points: " + pointPlay + " Computer points: " + pointComp);
+    }
+    if (pointPlay > pointComp) {
+      return ("The winner is You! Player: " + pointPlay + ". Computer: " + pointComp);
+    }
+    else {
+      return ("The winner is the Computer: " + pointComp + ". Player: " + pointPlay);
     }
   }
 
-  let winner = game();
+  let winner = console.log(game());
