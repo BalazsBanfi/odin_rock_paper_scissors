7ddc4eba691eb8e3d6b839b7167d167fe26f9d98
bally4h <banfibalazs81@gmail.com>
2023-03-17 15:03:14 +0100

    No console log, winner to textContent, 1í wins win

--- myscript.js
+++ myscript.js
@@ -2,66 +2,69 @@ let pointPlayer = 0;
 let pointComputer = 0;
 let playerSelection = "";
 let computerSelection = "";
-let won = "";
+const divResult = document.querySelector('#result');
 
 // The computer choose random
 function getComputerChoice() {
-    let choice = "";
-    switch (Math.floor(Math.random() * 3)) {
-        case 0:
-          choice = "Rock";
-          break;
-        case 1:
-          choice = "Paper";
-          break;
-        case 2:
-          choice = "Scissors";
-    }
-    return choice;
+  let choice = "";
+  switch (Math.floor(Math.random() * 3)) {
+    case 0:
+      choice = "Rock";
+      break;
+    case 1:
+      choice = "Paper";
+      break;
+    case 2:
+      choice = "Scissors";
+  }
+  return choice;
 }
 
 
 // One round player vs computer
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-      return "tie";
-    }
-    if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
-      (playerSelection === "Paper" && computerSelection === "Rock") ||
-      (playerSelection === "Scissors" && computerSelection === "Paper")) {
-        return "player";
-    }
-    else {
-      return "computer";
-    }
+  if (playerSelection === computerSelection) {
+    return "tie";
+  }
+  if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper")) {
+      return "player";
+  }
+  else {
+    return "computer";
+  }
 }
-
 
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((butt) => {
-    butt.addEventListener('click', () => {
-        computerSelection = getComputerChoice();
-        playerSelection = butt.value;
-        winner = playRound(playerSelection, computerSelection);
-        if (winner == "player") {
-            pointPlayer++;
-        }
-        if (winner == "computer") {
-            pointComputer++;
-        }
+  butt.addEventListener('click', () => {
+    computerSelection = getComputerChoice();
+    playerSelection = butt.value;
+    winner = playRound(playerSelection, computerSelection);
+    if (winner == "player") {
+      pointPlayer++;
+      divResult.textContent = 'Player won';
+    }
+    if (winner == "computer") {
+      pointComputer++;
+      divResult.textContent = 'Computer won';
+    }
+    
+    if (winner == "tie") {
+      divResult.textContent = 'Tie';
+    }
 
-        console.log("Player: ", pointPlayer, " Computer: ", pointComputer);            
-
-        if (pointPlayer === 10) {
-            console.log("The winner is the Player, player points: ", pointPlayer, ", computer points: ", pointComputer);
-            pointComputer = 0;
-            pointPlayer = 0;
-        }
-        if (pointComputer === 10) {
-            console.log("The winner is the Computer, computer points: ", pointComputer, ", player points: ", pointPlayer);
-            pointComputer = 0;
-            pointPlayer = 0;
-        }
-    })
+    if (pointPlayer === 10) {
+      divResult.textContent = 'The winner is the Player!';
+      pointComputer = 0;
+      pointPlayer = 0;
+    }
+    if (pointComputer === 10) {
+      divResult.textContent = 'The winner is the Player!';
+      pointComputer = 0;
+      pointPlayer = 0;
+    }
+  })
 });
